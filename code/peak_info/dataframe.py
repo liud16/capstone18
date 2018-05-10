e0f9922ee1b92071e7703069666f8eedb001ab06
jing <tuj2@uw.edu>
2018-05-10 00:21:26 -0700

    peak dataframe

---
+++ code/peak_info/dataframe.py
@@ -0,0 +1,229 @@
+# importing the libraries
+import numpy as np
+import peakutils
+import syntheticdata
+import threegaussians
+import lorentzian
+from peakutils.plot import plot as pplot
+from matplotlib import pyplot as plt
+%matplotlib inline
+from scipy.optimize import curve_fit
+from scipy import interpolate
+from astropy.modeling import models, fitting
+import pandas as pd
+
+# define smoothing function
+def astropy_Smoothing(nm_array,timedelay,noise_coefficient,gg_init):
+    # Generate fake data
+    np.random.seed(42)
+    ydata = timedelay + noise_coefficient*np.random.normal(size=nm_array.size)
+    # Now to fit the data create a new superposition with initial
+    # guesses for the parameters:
+    fitter = fitting.SLSQPLSQFitter()
+    gg_fit = fitter(gg_init, nm_array, ydata)
+  
+    return gg_fit(nm_array)
+
+ def earth_Smoothing(nm_array, y_array,noise_coefficient):        
+    """
+    ============================================
+     Plotting derivatives of simple sine function
+    ============================================
+
+     A simple example plotting a fit of the sine function
+    and the derivatives computed by Earth.
+    
+    Notes
+    -----   
+    generates a denoise curve from the TA data
+    Parameters
+    ----------
+        nm_array: wavelength array
+        timedelay: time delay array
+        noise_coefficient: the noise coefficients that user want to generate
+    Returns
+    -------
+        a smoothing curve from the original noise curve   
+    """
+    from pyearth import Earth
+   # Fit an Earth model
+    model = Earth(smooth=True)
+    np.random.seed(42)
+    ydata = y_array + noise_coefficient*np.random.normal(size=nm_array.size)
+    model.fit(nm_array, ydata)
+
+   # Print the model
+    #print(model.trace())
+    #print(model.summary())
+
+   # Get the predicted values and derivatives
+    y_hat = model.predict(nm_array)
+    
+    return  y_hat
+
+# get noise and astropy-smoothing dataframe
+def astropy_smooth_matrix(nm_array,data_matrix,noise_coefficient,gg_init):
+    num_array = np.shape(data_matrix)[0]
+    
+    smooth_matx = pd.DataFrame(np.empty((num_array,1)), columns = ['a'])
+    noise_matx = pd.DataFrame(np.empty((num_array,1)), columns = ['a'])
+    
+    for i in range(num_array):
+        data_array = data_matrix[:, i]
+        
+        # get noise and smooth list
+        noise_array = add_noise(nm_array, data_array, noise_coefficient).tolist()
+        smooth_array = astropy_Smoothing(nm_array,data_array,noise_coefficient,gg_init).tolist()
+        
+        # get noise dataframe
+        DF = pd.DataFrame(noise_array,columns = [i])
+        noise_matx = noise_matx.join(DF)
+        
+        # get smooth dataframe
+        df = pd.DataFrame(smooth_array,columns = [i])
+        smooth_matx = smooth_matx.join(df)
+        
+    # drop the first columns  
+    noise_matx = noise_matx.drop(columns='a')
+    smooth_matx = smooth_matx.drop(columns='a')
+        
+    return noise_matx, smooth_matx
+
+# get noise and earth-smoothing dataframe 
+def earth_smooth_matrix(nm_array,data_matrix,noise_coefficient):
+    num_array = np.shape(data_matrix)[0]
+    
+    smooth_matx = pd.DataFrame(np.empty((num_array,1)), columns = ['a'])
+    noise_matx = pd.DataFrame(np.empty((num_array,1)), columns = ['a'])
+    
+    for i in range(num_array):
+        data_array = data_matrix[:, i]
+        
+        # get noise and smooth list
+        noise_array = add_noise(nm_array, data_array, noise_coefficient).tolist()
+        smooth_array =earth_Smoothing(nm_array,data_array,noise_coefficient).tolist()
+        
+        # get noise dataframe
+        DF = pd.DataFrame(noise_array,columns = [i])
+        noise_matx = noise_matx.join(DF)
+        
+        # get smooth dataframe
+        df = pd.DataFrame(smooth_array,columns = [i])
+        smooth_matx = smooth_matx.join(df)
+        
+    # drop the first columns  
+    noise_matx = noise_matx.drop(columns='a')
+    smooth_matx = smooth_matx.drop(columns='a')
+        
+    return noise_matx, smooth_matx
+
+ # get astropy-peak position
+ def astropy_peak_matrix(nm_array,data_matrix,noise_coefficient,threshold, min_dist,gg_init):
+    num_array = np.shape(data_matrix)[1]
+    true_peak = []
+    smooth_peak = []
+    
+    for i in range(num_array):
+        data_array = data_matrix[:, i]
+        noise_array = add_noise(nm_array, data_array, noise_coefficient)
+        smooth_array = astropy_smoothing(nm_array, data_array,noise_coefficient,gg_init)
+        
+        # get true peak matrix
+        indexes=findpeak(data_array, threshold, min_dist).tolist()
+        true_peak.append(indexes)
+        #get smooth peak matrix
+        
+        indexes1=findpeak(smooth_array, threshold, min_dist).tolist()
+        smooth_peak.append(indexes1)
+        
+        # transfer to dataframe
+        true_df=pd.DataFrame(true_peak)
+        smooth_df=pd.DataFrame(smooth_peak)
+    
+    return true_df, smooth_df
+
+ # get earth-peak position
+ def earth_peak_matrix(nm_array,data_matrix,noise_coefficient,threshold, min_dist):
+    num_array = np.shape(data_matrix)[1]
+    
+    true_peak = []
+    smooth_peak = []
+    
+    for i in range(num_array):
+        data_array = data_matrix[:, i]
+        noise_array = add_noise(nm_array, data_array, noise_coefficient)
+        smooth_array = Earth_Smoothing(nm_array, data_array,noise_coefficient)
+        
+        indexes=findpeak(data_array, threshold, min_dist).tolist()
+        true_peak.append(indexes)
+        
+        indexes1=findpeak(smooth_array, threshold, min_dist).tolist()
+        smooth_peak.append(indexes1)
+                
+        # transfer to dataframe
+        true_df=pd.DataFrame(true_peak)
+        smooth_df=pd.DataFrame(smooth_peak)
+    
+    return true_df, smooth_df
+
+
+ # get peak height and fwhm info
+ def peakchar(data_nm, data_z_array, peak_index):
+    """find the peak width, and intensity"""
+    num_peaks = len(peak_index)
+    
+    #array of peak height
+    height = [data_z_array[idx] for idx in peak_index]
+    
+    #array of peak width
+    half_height = [ht / 2 for ht in height]
+
+    fwhm_idx_1 = np.empty_like(half_height)
+    fwhm_idx_2 = np.empty_like(fwhm_idx_1)
+    fwhm_nm_1 = np.empty_like(fwhm_idx_1)
+    fwhm_nm_2 = np.empty_like(fwhm_idx_1)
+    
+    for i in range(num_peaks):
+        #find the index and nmof the left side of the fwhm
+        if i == 0:
+            fwhm_idx_1[i] = find_nearest(data_z_array[0:peak_index[i]], half_height[i])
+        else:
+            fwhm_idx_1[i] = find_nearest(data_z_array[peak_index[i-1]:peak_index[i]], half_height[i]) + peak_index[i-1]
+
+        fwhm_nm_1[i] = data_nm[int(fwhm_idx_1[i])]
+        
+        #find the index and nm of the right side of the fwhm   
+        fwhm_idx_2[i] = find_nearest(data_z_array[peak_index[i]:], half_height[i]) + peak_index[i]
+
+        fwhm_nm_2[i] = data_nm[int(fwhm_idx_2[i])]
+    
+    #find fwhm
+    fwhm = fwhm_nm_2 - fwhm_nm_1
+
+    return height, fwhm
+
+
+ def peak_matrix(nm_array,data_matrix, threshold, mindist):
+    """find peaks in a data matrix"""
+    peak_idx_matx = []
+    peak_height_matx = []
+    peak_fwhm_matx = []
+    
+    for i in range(num_timeslice):
+        data_timeslice = data_matrix[:, i]
+        
+        peak_idx = findpeak(data_timeslice, threshold, mindist).tolist()
+        peak_idx_matx.append(peak_idx)
+        
+        
+        peak_height, peak_fwhm = peakchar(nm_array, data_timeslice, peak_idx)
+        
+        peak_height_matx.append(peak_height)
+        peak_fwhm_matx.append(peak_fwhm)
+        
+        # transfer to dataframe
+        peak_idx_df=pd.DataFrame(peak_idx_matx)
+        peak_height_df=pd.DataFrame(peak_height_matx)
+        peak_fwhm_df=pd.DataFrame(peak_fwhm_matx)
+        
+    return peak_idx_df, peak_height_df, peak_fwhm_df
