587b70842862ba5bd3de5735ffb4fb62b391ac54
Demi Liu <demiliu@uw.edu>
2018-06-08 14:16:35 +0800

    add documentation

--- peakaboo/data_smoothing.py
+++ peakaboo/data_smoothing.py
@@ -5,19 +5,46 @@ import pandas as pd
 
 
 def find_nearest(array,value):
+    """
+    find the index of an item in an array that is closest to a given value
+    
+    Args:
+        array: numpy array
+        value: int or float
+    
+    Returns:
+        int
+        
+    """
+    
     idx = (np.abs(array-value)).argmin()
     return idx
 
 
 # load .txt data
 def load_data(data_filename):
-    """load matrix data"""
+    """
+    load matrix data
+    
+    Args:
+        data_filename: string
+        
+    Returns:
+        data_nm: an array of wavelengths, numpy array
+        data_time: an array of time-delay, numpy array
+        data_z: a 2-way matrix, numpy matrix
+    
+    """
     data = np.genfromtxt(data_filename, delimiter='\t')
-    data_nm = data[1:,0]    #wavelength in nm
+    data_nm = data[1:,0]
     data_time = data[0,1:]
     data_z = data[1:, 1:]
 
-    return data_nm, data_time, data_z
+    data_nm_use = data_nm[find_nearest(data_nm, startnm):find_nearest(data_nm, endnm)]
+    data_z_use = data_z[find_nearest(data_nm, startnm):find_nearest(data_nm, endnm), :]
+
+    return data_nm_use, data_time, data_z_use
+
 
 # load .csv data
 def load_data_csv(data_filename, startnm, endnm):
@@ -36,23 +63,16 @@ def load_data_csv(data_filename, startnm, endnm):
 
 def earth_smoothing(nm_array, y_array):
     """
-    =============================================
     Smoothen noisy data using py-earth,
     based on multivariate adaptive regression spline
-    =============================================
-
-
-    Notes
-    -----
-    generates a de-noised curve from the TA data
     
-    Parameters
-    ----------
+    Args:
         nm_array: wavelength array
         y-array: intensity array
-    Returns
-    -------
+        
+    Returns:
         a smoothing curve from the original noise curve
+    
     """
    # Fit an Earth model
     model = Earth(smooth=True)
@@ -70,19 +90,30 @@ def earth_smoothing(nm_array, y_array):
 
 
 def earth_smooth_matrix(nm_array, data_matrix):
+    """
+    Smoothen each time-slice in a data matrix
+    
+    Args:
+        nm_array: wavelength array
+        data_matrix: two-way data
+    
+    Returns:
+        smooth_matx: smoothened data matrix
+    
+    """
+    
     num_array = np.shape(data_matrix)[1]
     smooth_matx = pd.DataFrame(np.empty((len(nm_array),1)), columns = ['a'])
 
-    #print (num_array)
     for i in range(num_array):
         data_array = data_matrix[:, i]
         smooth_array = earth_smoothing(nm_array, data_array).tolist()
 
-        # get smooth dataframe
+        # add smoothened timeslice data to the matrix
         df = pd.DataFrame(smooth_array, columns = [i])
         smooth_matx = smooth_matx.join(df)
 
-    # drop the first columns
+    # drop the first columns in the matrix
     smooth_matx = smooth_matx.drop(columns='a')
 
     return smooth_matx
