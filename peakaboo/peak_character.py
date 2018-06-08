760e96c44ec0740c1566889885eb87618a184549
Demi Liu <demiliu@uw.edu>
2018-06-08 15:10:22 +0800

    add documentation

--- peakaboo/peak_character.py
+++ peakaboo/peak_character.py
@@ -8,7 +8,19 @@ def find_nearest(array,value):
 
  # get peak height and fwhm info
 def peakchar(data_nm, data_z_array, peak_index):
-    """find the peak width, and intensity"""
+    """
+    find the peak height and width
+    
+    Args:
+        data_nm: wavelength array, numpy array
+        data_z_array: data array
+        peak_index: index of the peaks identified
+    
+    Returns:
+        height: peak intensity, numpy array
+        fwhm: widths pf peaks defined as full-width half-max, numpy array
+        
+    """
     num_peaks = len(peak_index)
     
     #array of peak height
@@ -23,7 +35,8 @@ def peakchar(data_nm, data_z_array, peak_index):
     fwhm_nm_2 = np.empty_like(fwhm_idx_1)
     
     for i in range(num_peaks):
-        #find the index and nmof the left side of the fwhm
+        #find the index and nm corresponding to half of the peak intensity
+        #on the left side of the peak
         if i == 0:
             fwhm_idx_1[i] = find_nearest(data_z_array[0:peak_index[i]], half_height[i])
         else:
@@ -31,20 +44,36 @@ def peakchar(data_nm, data_z_array, peak_index):
 
         fwhm_nm_1[i] = data_nm[int(fwhm_idx_1[i])]
         
-        #find the index and nm of the right side of the fwhm   
+        #find the index and nm corresponding to half of the peak intensity
+        #on the right side of the peak   
         fwhm_idx_2[i] = find_nearest(data_z_array[peak_index[i]:], half_height[i]) + peak_index[i]
 
         fwhm_nm_2[i] = data_nm[int(fwhm_idx_2[i])]
     
-    #find fwhm
+    #calculate fwhm as the difference between the index/nm's on the left and
+    #right side of the peak
     fwhm = fwhm_nm_2 - fwhm_nm_1
 
     return height, fwhm
 
 
 def peak_matrix(nm_array,data_matrix, threshold, mindist):
-    """find peaks in a data matrix
-    and calculate the height and width of the peaks"""
+    """
+    find peaks in a data matrix
+    and calculate the height and width of the peaks
+    
+    Args:
+        nm_array: wavelength array
+        data_matrix: two-way matrix
+        threshold: threshold of normalized peak intensity to be identified
+            as a peak, float between 0. and 1.
+        mindist: minimum distance between two peaks, int
+    
+    Returns:
+        three matrice that contains arrays of peak indices, 
+        peak heights and peak fwhm of each time-slice
+    
+    """
     
     peak_idx_matx = []
     peak_height_matx = []
