ae27bea7a23fdcda42c90bc7986db1d2220e3796
Ian Murphy <imurphy@uw.edu>
2018-06-22 20:52:30 -0700

    Classifier: Set threshold for intensity to 0.001. Visualizer: Increased figure size, added grids, increased axis label font size and thickness. Notebook has examples of this for exp06.

--- peakaboo/feature_visualizer.py
+++ peakaboo/feature_visualizer.py
@@ -1,23 +1,38 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+from numpy.polynomial.polynomial import polyfit
 
-def visualize(peak_dict):
+def visualize(peak_dict, data_nm):
     for i in range(len(peak_dict)):
         df = pd.DataFrame(peak_dict['peak_%s' % i], 
         columns=['Position', 'Height', 'Width', 'Time'])
+        df = df.drop_duplicates(subset= 'Time')
+        df = df.reset_index(drop=True)
+        height_norm = np.linalg.norm(df['Height'], keepdims=True)
+        fit_params, exp_fit, data, time = fit_single_exp_diffev(df['Time'], df['Height'])
         
-        plt.subplot(3, 1, 1)
-        plt.plot(df['Time'], df['Height'])
-        plt.title('Peak %s Dynamics' % (i+1))
-        plt.ylabel('Intensity')
-
-        plt.subplot(3, 1, 2)
-        plt.plot(df['Time'], df['Position'])
-        plt.ylabel('Position')
-
-        plt.subplot(3, 1, 3)
-        plt.plot(df['Time'], df['Width'])
-        plt.ylabel('Width')
-        plt.xlabel('Time')
-        plt.show()
+        fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(12, 8))
+        
+        ax1.plot(df['Time'], df['Height'], '.')
+        #ax1.plot(exp fit)
+        #ax1.axis(between 0 and 1?)
+        ax1.set_title('Peak %s Dynamics' % (i+1), fontsize=18, fontweight='bold')
+        ax1.set_ylabel('Intensity', fontsize=18, fontweight='bold')
+        ax1.grid()
+        
+        ax2.plot(df['Time'], df['Position'], '.')
+        ax2.plot(np.unique(df['Time']), np.poly1d(np.polyfit(df['Time'], df['Position'], 1))
+        (np.unique(df['Time'])))
+        ax2.set_ylabel('Position', fontsize=18, fontweight='bold')
+        ax2.set_ylim((0, data_nm.shape[0]))
+        ax2.grid()
+        
+        ax3.plot(df['Time'], df['Width'], '.')
+        ax3.plot(np.unique(df['Time']), np.poly1d(np.polyfit(df['Time'], df['Width'], 1))
+        (np.unique(df['Time'])))
+        ax3.set_ylabel('Width', fontsize=18, fontweight='bold')
+        ax3.set_xlabel('Time', fontsize=18, fontweight='bold')
+        ax3.grid()
+        
     return
