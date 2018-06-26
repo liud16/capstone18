f9e83c782cab65799e3485ffce3dc86f647a55a5
Demi Liu <demiliu@uw.edu>
2018-06-26 17:59:13 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ import os
 from setuptools import setup, find_packages
 PACKAGES = find_packages()
 
-# Get version and release info, which is all stored in shablona/version.py
-ver_file = os.path.join('peakaboo/peakaboo/version.py')
+# Get version and release info, which is all stored in peakaboo/version.py
+ver_file = 'peakaboo/version.py'
 with open(ver_file) as f:
     exec(f.read())
 
@@ -29,7 +29,7 @@ opts = dict(name=NAME,
             platforms=PLATFORMS,
             version=VERSION,
             packages=PACKAGES,
-            package_data=PACKAGE_DATA,
+            package_data=PACKAGE_DATA,            
             install_requires=REQUIRES,
             requires=REQUIRES)
 
