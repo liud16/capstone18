f26268c7ba6d53cc3e99a3b5e14b75731262feac
jing <tuj2@uw.edu>
2018-06-27 16:48:46 -0700

    py.doc

---
+++ peakaboo/test/smoothing.py
@@ -0,0 +1,90 @@
+ # importing the libraries
+import numpy as np
+from scipy.optimize import curve_fit
+from scipy import interpolate
+import pandas as pd
+
+# load some data
+def loaddata(data_filename):
+    """load matrix data"""
+    data = np.genfromtxt(data_filename, delimiter='\t')
+    data_nm = data[1:,0]    #wavelength in nm
+    data_time = data[0,1:]
+    data_z = data[1:, 1:]
+    
+    return data_nm, data_time, data_z
+
+# add noise
+def add_noise(nm_array, y_array, noise_coefficient):
+    # Add noise
+    np.random.seed(1800)
+    y_noise = noise_coefficient * np.random.normal(size=nm_array.size)
+    y_proc = y_array + y_noise
+    
+    return y_proc
+
+
+def earth_Smoothing(nm_array, y_array,noise_coefficient):
+    """
+        ============================================
+        Plotting derivatives of simple sine function
+        ============================================
+        
+        A simple example plotting a fit of the sine function
+        and the derivatives computed by Earth.
+        
+        Notes
+        -----
+        generates a denoise curve from the TA data
+        Parameters
+        ----------
+        nm_array: wavelength array
+        timedelay: time delay array
+        noise_coefficient: the noise coefficients that user want to generate
+        Returns
+        -------
+        a smoothing curve from the original noise curve
+        """
+    from pyearth import Earth
+    # Fit an Earth model
+    model = Earth(smooth=True)
+    np.random.seed(42)
+    ydata = y_array + noise_coefficient*np.random.normal(size=nm_array.size)
+    model.fit(nm_array, ydata)
+    
+    # Print the model
+    #print(model.trace())
+    #print(model.summary())
+    
+    # Get the predicted values and derivatives
+    y_hat = model.predict(nm_array)
+    
+    return  y_hat
+
+
+def earth_smooth_matrix(nm_array,data_matrix,noise_coefficient):
+    num_array = np.shape(data_matrix)[1]
+    
+    smooth_matx = pd.DataFrame(np.empty((num_array,1)), columns = ['a'])
+    noise_matx = pd.DataFrame(np.empty((num_array,1)), columns = ['a'])
+    
+    for i in range(num_array):
+        data_array = data_matrix[:, i]
+        
+        # get noise and smooth list
+        noise_array = add_noise(nm_array, data_array, noise_coefficient).tolist()
+        smooth_array =earth_Smoothing(nm_array,data_array,noise_coefficient).tolist()
+        
+        # get noise dataframe
+        DF = pd.DataFrame(noise_array,columns = [i])
+        noise_matx = noise_matx.join(DF)
+        
+        # get smooth dataframe
+        df = pd.DataFrame(smooth_array,columns = [i])
+        smooth_matx = smooth_matx.join(df)
+    
+    # drop the first columns
+    noise_matx = noise_matx.drop(columns='a')
+    smooth_matx = smooth_matx.drop(columns='a')
+
+    return noise_matx, smooth_matx
