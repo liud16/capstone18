d3425c63bdb8f49872196a3d851986fd54703ea6
Demi Liu <demiliu@uw.edu>
2018-06-27 09:07:54 +0800

    moved remove_outlier to development

--- development/remove_outlier.py
+++ development/remove_outlier.py
@@ -11,16 +11,30 @@ from sklearn.isotonic import IsotonicRegression
 
 
 def id_outliers_replacewith_interp(x_array, data, m, win_len):
+    """
+    identify outliers in an array of peak positions
+    
+    Args:
+        x_array: time array, numpy array
+        data: 1D wavelength or index array, numpy array
+        m: scale of standard deviation used to identify outlier,
+            float between 0. and 1.
+        win_len: window size to search for outlier
+    
+    Returns:
+        new_data_final_interp: a new data array that's absent of outliers
+        
+    """
+    
     reshape_x_array = []
     reshape_data = []
     quotient_array = np.empty(len(data))
     remainder_array = np.empty(len(data))
     quotient_array[0] = 0
     remainder_array[0] = 0  
-    #print divmod(len(data), win_len)   
+
     quotient_max = divmod(len(data), win_len)[0]
-    print (quotient_max)
-    #quotient_array_new = []
+
     data_idx = np.arange(0, len(data), 1)
     for i in range(1, len(data_idx)):
         
@@ -39,12 +53,14 @@ def id_outliers_replacewith_interp(x_array, data, m, win_len):
         else:
             pass
     quotient_max_idx = np.where(quotient_array == quotient_max)
-    #print quotient_max_idx
+
     reshape_data.append(data[quotient_max_idx[0]])
     reshape_x_array.append(x_array[quotient_max_idx[0]])
-    #print reshape_data
+ 
     reshape_data_shape = np.shape(reshape_data)[0]
-    #print reshape_data_shape
+
+    #identifies outliers by comparing the value with the mean
+    #and deletes 
     def id_outliers_and_delete(d,x, m):
         d_mean = np.mean(d)  
         d_stdev = np.std(d)
@@ -62,25 +78,42 @@ def id_outliers_replacewith_interp(x_array, data, m, win_len):
         d_delete = np.delete(d, outlier_idx)
         x_delete = np.delete(x, outlier_idx)
         
-        #print data2[outlier_idx]
         return x_delete, d_delete
     
     new_x_array = []
     new_data = []
     for i in range(reshape_data_shape):
-        new_data.append(id_outliers_and_delete(reshape_data[i],reshape_x_array[i], 1)[1])#(id_outliers_replacewith_mean(reshape_data[i], m))
-        new_x_array.append(id_outliers_and_delete(reshape_data[i],reshape_x_array[i],1)[0])
-    new_data_flat = np.concatenate(new_data[:-1]).ravel().tolist()#.flatten()
-    new_x_array_flat = np.concatenate(new_x_array[:-1]).ravel().tolist()#.flatten()
-    new_data_final = np.concatenate((new_data_flat, new_data[reshape_data_shape - 1]))
-    new_x_array_final = np.concatenate((new_x_array_flat, new_x_array[reshape_data_shape - 1]))
-    
+        new_data.append(id_outliers_and_delete
+                        (reshape_data[i],reshape_x_array[i], 1)[1])
+        new_x_array.append(id_outliers_and_delete
+                           (reshape_data[i],reshape_x_array[i],1)[0])
+    new_data_flat = np.concatenate(new_data[:-1]).ravel().tolist()
+    new_x_array_flat = np.concatenate(new_x_array[:-1]).ravel().tolist()
+    new_data_final = np.concatenate(
+            (new_data_flat, new_data[reshape_data_shape - 1]))
+    new_x_array_final = np.concatenate(
+            (new_x_array_flat, new_x_array[reshape_data_shape - 1]))
+
+    #generates a new data array by interpolating the data array after 
+    #removing outliers over the original x-array    
     new_data_final_interp = np.interp(x_array, new_x_array_final, new_data_final)    
     
     return new_data_final_interp
 
 
 def isotonic(x, y):
+    """
+    predict y-array based on isotonic regression model
+    
+    Args:
+        x: time array, numpy array
+        y: peak position or nm, numpy array
+    
+    Returns:
+        y_: the isotonic transform based on the given time array and peak array
+    
+    """
+    
     ir = IsotonicRegression()
     y_ = ir.fit_transform(x, y)
     
