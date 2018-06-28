95ababd78e3c73fd6be8c5b1c95dfcde7b54435f
Demi Liu <demiliu@uw.edu>
2018-06-28 22:32:44 +0800

    flake8

--- peakaboo/tests/test_data_smoothing.py
+++ peakaboo/tests/test_data_smoothing.py
@@ -8,6 +8,7 @@ Created on Thu Jun 28 21:49:38 2018
 
 from peakaboo.data_smoothing import load_data_csv, load_data
 
+
 def test_load_data_csv():
     file_1 = 1
     try:
@@ -16,7 +17,7 @@ def test_load_data_csv():
         pass
     else:
         'TypeError not handled'
-    
+
     file_2 = 'test.csv'
     try:
         load_data_csv(file_2, '1', 1400, 1)
@@ -24,29 +25,28 @@ def test_load_data_csv():
         pass
     else:
         'TypeError not handled'
-        
+
     try:
         load_data_csv(file_2, 700, '5', 1)
     except AssertionError:
         pass
     else:
         'TypeError not handled'
-        
+
     try:
         load_data_csv(file_2, 700, 1400, '1')
     except TypeError:
         pass
     else:
         'TypeError not handled'
-        
-        
+
     try:
         load_data_csv(file_2, 700, 1400, 1)
     except AssertionError:
         pass
     else:
         'ValueError not handled'
-    
+
     try:
         load_data_csv(file_2, 900, 1800, 1)
     except AssertionError:
@@ -60,7 +60,7 @@ def test_load_data_csv():
         pass
     else:
         'ValueError not handled'
-    
+
     return
 
 
@@ -72,7 +72,7 @@ def test_load_data():
         pass
     else:
         'TypeError not handled'
-    
+
     file_2 = 'test.txt'
     try:
         load_data(file_2, '1', 1400, 1)
@@ -80,29 +80,28 @@ def test_load_data():
         pass
     else:
         'TypeError not handled'
-        
+
     try:
         load_data(file_2, 700, '5', 1)
     except AssertionError:
         pass
     else:
         'TypeError not handled'
-        
+
     try:
         load_data(file_2, 700, 1400, '1')
     except TypeError:
         pass
     else:
         'TypeError not handled'
-        
-        
+
     try:
         load_data(file_2, 700, 1400, 1)
     except AssertionError:
         pass
     else:
         'ValueError not handled'
-    
+
     try:
         load_data(file_2, 900, 1800, 1)
     except AssertionError:
@@ -116,5 +115,5 @@ def test_load_data():
         pass
     else:
         'ValueError not handled'
-    
+
     return
