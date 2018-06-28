7b61c3eed821c3afdc2450b0f4a797896881ec04
Ian Murphy <imurphy@uw.edu>
2018-06-27 23:06:35 -0700

    Removed text from __init__.py that caused flake8 error. Moved jing's test WIP to code_dev/test/ dir.

---
+++ code_dev/test/readme.md
@@ -0,0 +1,5 @@
+----------------------------------------------------------------------
+Ran 7 tests in 972.062s
+
+OK
+toseis-MacBook-Pro:Peak-Testing tutu$ 
--- peakaboo/__init__.py
+++ peakaboo/__init__.py
@@ -1,9 +1,1 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jun 26 16:53:31 2018
 
-@author: demiliu
-"""
-
-from .version import __version__
